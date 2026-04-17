1fd75ff04a164ee1a1231b1fd1757ce0f0aab50f
yuduru910kindle-ops <yuduru910kindle@gmail.com>
2026-04-17 16:01:47 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'my-apps-v1';
+const FILES = [
+  '/my-apps/個人支出まとめ.html',
+  '/my-apps/出張費まとめ.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
